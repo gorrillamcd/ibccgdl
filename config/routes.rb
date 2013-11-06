2c6a81ad628e88e6caf400c74eca8958b53546f2
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2013-11-06 23:37:47 +0000

    Got the signup page view started with background image

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Ibccgdl::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#signup'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
